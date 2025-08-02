017fbf13681e2dee1af8baf57c5e730ab61d157e
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-08-02 14:18:26 -0600

    fix for build

--- frontend/src/components/UpdateMovieForm.tsx
+++ frontend/src/components/UpdateMovieForm.tsx
@@ -1,3 +1,4 @@
+/*
 // src/components/UpdateMovieForm.tsx
 'use client';
 import { apiURL } from '@/services/services';
@@ -120,3 +121,4 @@ export default function UpdateMovieForm({ id, initialData }: UpdateMovieFormProp
     </>
   );
 }
+  */
